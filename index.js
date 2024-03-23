6912854143a495a20b383d813b3f81a921a8c948
KiraTeresa <kira.soba@gmx.de>
2024-03-23 19:16:20 +0100

    configure cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+const PORT = 8080;
+
+app.use(cors({origin: 'http://localhost:3000'}))
 
 app.get('/', (req, res) => {
-	res.send('Hello');
+	console.log(`Request received from '${req.hostname}'`);
+	res.json({message: 'you got it'});
 })
 
-app.listen(5000, () => {
-	console.log('Listening on port 5000');
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
 })
