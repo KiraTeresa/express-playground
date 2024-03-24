bd4b2df6b8e62ee99dd02f5de3a514d9ba2e6ae6
Kira <kira.soba@gmx.de>
2024-03-24 10:57:17 +0100

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = 8080;
+const PORT = 3000;
 
 app.use(cors({origin: 'http://localhost:3000'}))
 
