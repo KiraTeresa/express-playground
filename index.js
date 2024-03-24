2791a95b82fb1f50d97257d3c204d149aa650642
Kira <kira.soba@gmx.de>
2024-03-24 14:30:20 +0100

    change port to 8080

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(cors({origin: 'http://localhost:3000'}))
 
 app.get('/', (req, res) => {
 	console.log(`Request received from '${req.hostname}'`);
 	res.json({message: 'you got it'});
+})
+
+app.post('/csp-reporting', (req, res) => {
+	const report = req.body;
+	console.log({report});
 })
 
 app.listen(PORT, () => {
